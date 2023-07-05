1492740517b1115cd7edbf943cd26329ed26688a
raushan kuamr <sraushan890@gmail.com>
2023-07-05 21:05:34 +0530

    create task component done

--- Task-Manager/client/src/Pages/Home.jsx
+++ Task-Manager/client/src/Pages/Home.jsx
@@ -1,21 +1,61 @@
-import React from 'react'
-import styles from "../Styles/pages/home.module.css"
+import React, { useEffect, useState } from "react";
+import styles from "../Styles/pages/home.module.css";
+import { useDispatch } from "react-redux";
+import { createTaskApiCall } from "../Store/TaskAppReducer/action";
 function Home() {
+  const [createTask, setCreateTask] = useState("");
+  const [success, SetSuccess] = useState('');
+  const [error, SetError] = useState("");
+  const dispatch = useDispatch();
+  const handleSubmitFrom = (event) => {
+    event.preventDefault();
+    if (createTask) {
+      const payload={name:createTask}
+      dispatch(createTaskApiCall(payload)).then((res) => {
+        console.log(res)
+        if (res.payload?.success === true) {
+          SetSuccess("Task Created Successfully")
+          setTimeout(() => {
+            SetSuccess("")
+          },2000)
+        } else {
+          SetError(res.payload.msg)
+           setTimeout(() => {
+             SetError("");
+           }, 2000);
+        };
+      });
+      setCreateTask("")
+    }
+  };
+  useEffect(() => {}, []);
   return (
     <div className={styles.container}>
       <div className={styles.heading}>
         <h1>Task Manager</h1>
       </div>
-      <div className={styles.createTaskContainer}>
-        <form action="">
-          <input type="text" />
-          <button type='submit'>create</button>
-        </form>
-      </div>
+      <form
+        action=""
+        className={styles.createTaskContainer}
+        onSubmit={handleSubmitFrom}
+      >
+        <input
+          type="text"
+          name="task"
+          placeholder="Enter task here... "
+          value={createTask}
+          onChange={(e) => setCreateTask(e.target.value)}
+        />
+        <button type="submit">Add Task</button>
+        {success ? (
+          <p className={styles.success}>{ success}</p>
+        ) : (
+            <p className={styles.error}>{error}</p>
+        )}
+      </form>
       {/* all task component */}
-      
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
--- Task-Manager/client/src/Store/TaskAppReducer/action.js
+++ Task-Manager/client/src/Store/TaskAppReducer/action.js
@@ -1,2 +1,28 @@
 import * as types from "./action.type.js";
 import axios from "axios";
+
+// create task
+
+export const createTaskApiCall = (payload) => (dispatch) => {
+  dispatch({ type: types.CREATE_TASK_REQUEST });
+  return axios
+    .post("http://localhost:4000/api/v1/task", payload)
+    .then((res) =>
+     { return  dispatch({ type: types.CREATE_TASK_SUCCESS, payload: res.data })}
+  ).catch((err) => {
+      return dispatch({ type: types.CREATE_TASK_FAILURE, payload: err.data })
+  });
+};
+
+// get all task
+export const getAllTaskApiCall = () => (dispatch) => {
+    dispatch({ type: types.GET_ALL_TASK_REQUEST });
+    return axios
+      .get("http://localhost:4000/api/v1/task")
+      .then((res) => {
+        return dispatch({ type: types.GET_ALL_TASK_SUCCESS, payload: res.data });
+      })
+      .catch((err) => {
+        return dispatch({ type: types.GET_ALL_TASK_FAILURE, payload: err.data });
+      });
+}
